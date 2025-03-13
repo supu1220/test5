70f3e8a65e92c44360e3a986c515017cd70ca291
supu1220 <supusuprava@gmail.com>
2025-03-13 15:24:11 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -9,6 +9,7 @@ public class Test5Application {
 	public static void main(String[] args) {
                 String name="suprava";
                 int x=100;
+		int y=200;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
